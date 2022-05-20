520fc9c6e1a3db22aa35849d1e1fedf8d0e8ecb3
Manuel Scholz <manuel.serrano943@gmail.com>
2022-05-20 17:31:14 +0200

    Feat: API Excel, pintar letras que coinciden y coincidir aquellas con o sin tildes

---
+++ pages/api/excel.js
@@ -0,0 +1,23 @@
+import { google } from "googleapis";
+
+export async function fetchExcel() {
+    let result = [];
+    const target = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+    const jwt = new google.auth.JWT(
+        process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+        null,
+        (process.env.GOOGLE_SHEETS_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
+        target
+    );
+    const sheets = google.sheets({ version: "v4", auth: jwt });
+    const response = await sheets.spreadsheets.values.get({
+        spreadsheetId: process.env.SPREADSHEET_ID,
+        range: "Hoja 1",
+    });
+    const cols = response.data.values;
+    cols.shift();
+    cols.forEach(ingredient => {
+        result.push({name: ingredient[0], descr: ingredient[1]})
+    })
+    return result;
+}
