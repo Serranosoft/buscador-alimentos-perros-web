bc38f7dbfed465ba8444438d34445411cf3af4ba
Manuel Scholz <manuel.serrano943@gmail.com>
2022-05-22 17:40:08 +0200

    Feat: Metatag para search console

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
             <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;500;600;700;800;900&display=swap" rel="stylesheet" /> 
+            <meta name="google-site-verification" content="mDr4jX4s19KzRXyYUwYi_BB1gm2cb5AziPGwHqW8yI0" />
         </Head>
         <Header />
         <main className="mainContainer">
