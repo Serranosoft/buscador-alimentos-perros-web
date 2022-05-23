7db3eb50bb117e2e603afb8514695c7506c68e1d
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-05-23 12:18:59 +0200

    Feat: Metadatos en head

--- pages/buscador/alimento/[name].js
+++ pages/buscador/alimento/[name].js
@@ -1,21 +1,30 @@
 import { fetchIngredientByName } from "../../api/excel"
 import styles from '../../../styles/css/Ingredient.module.css'
 import { useEffect } from 'react';
+import Head from 'next/head'
 
 export default function Ingrediente(props) {
+    console.log(typeof(props.name));
 
     useEffect(() => {
         props.suitable ? document.getElementById("ingr-descr-container").style.backgroundColor = "#bbffe9" : document.getElementById("ingr-descr-container").style.backgroundColor = "#ffccd0"
     }, [props])
-
     return (
-        <section className={styles.container}>
-            <h1>¿Un perro puede comer <span className={styles.headingVariable}>{props.name}</span>?</h1>
-            <div id="ingr-descr-container" className={styles.descrContainer}>
-                <span>{props.descr}</span>
-            </div>
-            <span>{props.suitable}</span>
-        </section>
+        <>
+            <Head>
+                <title>¿Un perro puede comer {props.name}? Respuestas rápidas en BuscaDog</title>
+                <meta name="description" content={`¿Un perro puede comer ${props.name}? Respuestas rápidas en Buscadog`} />
+                <link rel="icon" href="/favicon.ico" />
+                <meta property="og:title" content={`¿Un perro puede comer ${props.name}? Respuestas rápidas en Buscadog`} />
+            </Head>
+            <section className={styles.container}>
+                <h1>¿Un perro puede comer <span className={styles.headingVariable}>{props.name}</span>?</h1>
+                <div id="ingr-descr-container" className={styles.descrContainer}>
+                    <span>{props.descr}</span>
+                </div>
+                <span>{props.suitable}</span>
+            </section>
+        </>
     )
 }
 
@@ -36,5 +45,6 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
     const { params } = context
     let result = await fetchIngredientByName(params.name);
+    console.log(result);
     return { props: result }
 }
