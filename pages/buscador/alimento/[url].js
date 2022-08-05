868d63c88e9f7c7cef4355fd942270461f06a59b
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-05 02:35:15 +0200

    Feat: Sugerencias con imagenes y urls, cambio de diseño en Ingredient

--- pages/buscador/alimento/[url].js
+++ pages/buscador/alimento/[url].js
@@ -37,20 +37,69 @@ export default function Ingrediente({ data }) {
                         <section>
                             <div>
                                 <div className={s.sidebar}>
-                                    <div className={s.notification}>
-                                        <span>Te recomendamos encarecidamente que visites un veterinario para poder analizar el caso de tu mascota.</span>
-                                    </div>
                                     <div className={s.suggestions}>
                                         <span>¿Mi perro puede comer?</span>
                                         <div>
-                                            <span>Cebolla</span>
-                                            <span>Pimientos</span>
-                                            <span>Calabacín</span>
-                                            <span>Arroz</span>
-                                            <span>Cebolla</span>
-                                            <span>Pimientos</span>
-                                            <span>Calabacín</span>
-                                            <span>Arroz</span>
+                                            <Link href={"/buscador/alimento/manzana"}>
+                                                <a target="_blank">
+                                                    <img src="/img/apple.svg" />
+                                                    <span>Manzana</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/platano"}>
+                                                <a target="_blank">
+                                                    <img src="/img/banana.svg" />
+                                                    <span>Plátano</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/brocoli"}>
+                                                <a target="_blank">
+                                                    <img src="/img/broccoli.svg" />
+                                                    <span>Brocoli</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/zanahoria"}>
+                                                <a target="_blank">
+                                                    <img src="/img/carrot.svg" />
+                                                    <span>Zanahoria</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/queso"}>
+                                                <a target="_blank">
+                                                    <img src="/img/cheese.svg" />
+                                                    <span>Queso</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/pollo"}>
+                                                <a target="_blank">
+                                                    <img src="/img/chicken.svg" />
+                                                    <span>Pollo</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/chocolate"}>
+                                                <a target="_blank">
+                                                    <img src="/img/chocolate-bar.svg" />
+                                                    <span>Chocolate</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/ketchup"}>
+                                                <a target="_blank">
+                                                    <img src="/img/hot-sauce.svg" />
+                                                    <span>Ketchup</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/naranja"}>
+                                                <a target="_blank">
+                                                    <img src="/img/orange.svg" />
+                                                    <span>Naranja</span>
+                                                </a>
+                                            </Link>
+                                            <Link href={"/buscador/alimento/sandia"}>
+                                                <a target="_blank">
+                                                    <img src="/img/watermelon.svg" />
+                                                    <span>Sandía</span>
+                                                </a>
+                                            </Link>
                                         </div>
                                     </div>
                                 </div>
@@ -77,7 +126,7 @@ export async function getStaticPaths() {
     const paths = data.map((ingr) => ({
         params: { url: ingr["url"].toLowerCase() }
     }))
-    
+
     return {
         paths,
         fallback: true
