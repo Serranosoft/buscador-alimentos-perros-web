2e82c7dc057948c9da2ed326bf792469bbb4d222
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-08-05 15:02:14 +0200

    Feat: Terminar diseño de [url]

--- pages/buscador/alimento/[url].js
+++ pages/buscador/alimento/[url].js
@@ -5,6 +5,7 @@ import { supabase } from '../../../utils/supabaseClient';
 import Loading from '../../../components/Loading';
 import { addSpaces } from '../../../utils/strings';
 import Suggestion from '../../../components/suggestions';
+import Link from 'next/link';
 
 export default function Ingrediente({ data }) {
     const [ready, setReady] = useState(false);
@@ -36,14 +37,25 @@ export default function Ingrediente({ data }) {
                     <main className={s.root}>
                         <section>
                             <div>
-                                <div className={s.sidebar}>
-                                    <Suggestion />
-                                </div>
                                 <div className={s.content}>
                                     <h1>¿Los perros pueden comer <span>{data.nombre}</span>?</h1>
                                     <div>
                                         <p className={s.description}><span>{data.respuesta} .</span>{data.descripcion}</p>
                                     </div>
+                                    <Link href="/buscador/alimento">
+                                        <a className={s.back}>
+                                            <div>
+                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M151.5 347.8L3.5 201c-4.7-4.7-4.7-12.3 0-17l19.8-19.8c4.7-4.7 12.3-4.7 17 0L160 282.7l119.7-118.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17l-148 146.8c-4.7 4.7-12.3 4.7-17 0z"></path></svg>
+                                            </div>
+                                            <span>Buscar otros alimentos</span>
+                                        </a>
+                                    </Link>
+                                    <div className={s.notification}>
+                                        <p>¡Te recomendamos visitar a un veterinario antes de tomar una decisión para obtener una recomendación cientifica y respaldada por un profesional!</p>
+                                    </div>
+                                </div>
+                                <div className={s.sidebar}>                                    
+                                    <Suggestion />
                                 </div>
                             </div>
                         </section>
