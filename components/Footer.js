0eb1ca31e8e18bed7966a485395af7f5dfd9fabf
Manuel Serrano Scholz <mserrano@surcontrol.com>
2022-08-05 14:01:54 +0200

    Fix: Ignorar case y subir ficheros actualizados para rutas de modulos en vercel

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+import s from '../styles/css/Footer.module.css'
+import Link from "next/link";
+
+export default function Footer() {    
+
+    return (
+        <footer className={s.footer}>
+            <Link href="/buscador/alimento/todos">
+                <a>
+                    Todos los alimentos
+                </a>
+            </Link>
+        </footer>
+    )
+}
---
+++ components/Loading.js
@@ -0,0 +1,14 @@
+import s from '../styles/css/LoadingBar.module.css';
+
+const Loading = () => {
+    return (
+        <div className={s.root}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+};
+
+export default Loading;
