8c66d2bcfea63e8fbf98d8b1afb03501c36d7ae1
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-13 15:45:30 +0200

    Feat: Agregar enlaces de politicas al footer

--- components/footer.js
+++ components/footer.js
@@ -6,8 +6,18 @@ export default function Footer() {
     return (
         <footer className={s.footer}>
             <Link href="/buscador/alimento/todos">
-                <a>
+                <a target="_blank">
                     Todos los alimentos
+                </a>
+            </Link>
+            <Link href="/politica-de-privacidad">
+                <a target="_blank">
+                    Politica de privacidad
+                </a>
+            </Link>
+            <Link href="/politica-de-cookies">
+                <a target="_blank">
+                    Política de cookies
                 </a>
             </Link>
         </footer>
