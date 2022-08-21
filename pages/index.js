af0757466f7f14f9f2657e6b24269673a9d58645
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-21 14:47:36 +0200

    Feat: Diseño home

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,13 @@ import Head from 'next/head'
 import s from '../styles/css/Home.module.css'
 import Link from "next/link";
 import CTA from '../components/CTA';
+import buscador from '../components/buscador';
+import BuscadorAlimento from '../components/buscador';
+import { supabase } from '../utils/supabaseClient';
+import DogHero from '../components/DogHero';
+import Image from "next/image";
 
-export default function Home() {
+export default function Home( {ingredients} ) {
 
     return (
         <>
@@ -14,34 +19,147 @@ export default function Home() {
                 <meta property="og:url" content="https://buscadog.net/" />
                 <meta name="twitter:description" content="¿Qué puede comer un perro? Información sobre Perros | BuscaDog"></meta>
             </Head>
-            <section className={s.mainSection}>
-                <div className={s.heroSection}>
-                    <span className={s.heroTitle}>Buscadores y herramientas para encontrar toda la información para tu perro</span>
-                    <p className={s.muted}>¿Preocupado por lo que puede o no puede comer tu perro?</p>
-                    <p className={s.muted}>Nuestro buscador interactivo te ayuda a encontrar información sobre que alimentos puede comer tu mascota.</p>
-                    <Link href="/buscador/alimento">
-                        <a>
-                            <CTA />
-                        </a>
-                    </Link>
-                </div>
-                <div className={s.videoSection}>
-                    <video playsInline autoPlay muted loop>
-                    <source type="video/mp4" src="/videos/hero-video.mp4" />
-                    </video>
-                </div>
-                <div className={s.heroGrid}>
-                    <h1>¿Qué puede comer un perro?</h1>
+
+            <section className={s.root}>
+
+                <div className={s.hero}>
                     <div>
                         <div>
-                            <span>Descúbre que puede comer tu mascota</span>
-                            <p>Mi perro ha cogido algo del suelo y no sabes si es bueno o malo para su aparato digestivo</p>
-                            <p>Buscadog te proporciona información para conocer como puede afectar en un perro comer ciertos productos.</p>
+                            <h1>¿Es saludable lo que come mi mascota?</h1>
+                            <span>Escribe aquí abajo un alimento orgánico o inorganico para saber si tu perro puede comerlo</span>
+                            <BuscadorAlimento ingredients={ingredients}/>
                         </div>
-                        <img src="/img/hero1.jpg" />
+                        <DogHero />
+                    </div>
+                </div>
+
+                <div className={s.popular}>
+                    <h2>Los alimentos mas buscados</h2>
+                    <div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Frutas</p>
+                                <Image src="/img/fruits.svg" width={50} height={50} alt="Frutas para perros" />
+                            </div>
+                            <div>
+                                <Link href="/buscador/alimento/manzana"><a target="_blank">Manzanas</a></Link>
+                                <Link href="/buscador/alimento/peras"><a target="_blank">Peras</a></Link>
+                                <Link href="/buscador/alimento/albaricoques"><a target="_blank">Albaricoques</a></Link>
+                                <Link href="/buscador/alimento/bananas"><a target="_blank">Banana</a></Link>
+                                <Link href="/buscador/alimento/fresas"><a target="_blank">Fresas</a></Link>
+                                <Link href="/buscador/alimento/granada"><a target="_blank">Granadas</a></Link>
+                                <Link href="/buscador/alimento/ciruelas"><a target="_blank">Ciruelas</a></Link>
+                                <Link href="/buscador/alimento/caqui"><a target="_blank">Caquis</a></Link>
+                            </div>
+                        </div>
+                        <div>
+                        <div className={s.popularTitle}>
+                                <p>Material Inorgánico</p>
+                                <Image src="/img/toilet-paper.svg" width={50} height={50} alt="Material inorganico para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a>Papel</a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                                <Link href="/"><a></a></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Verduras</p>
+                                <Image src="/img/vegetables.svg" width={50} height={50} alt="Verduras para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a target="_blank">Col</a></Link>
+                                <Link href="/"><a target="_blank">Ensalada</a></Link>
+                                <Link href="/"><a target="_blank">Eneldo</a></Link>
+                                <Link href="/"><a target="_blank">Espárragos blancos</a></Link>
+                                <Link href="/"><a target="_blank">Champiñones</a></Link>
+                                <Link href="/"><a target="_blank">Escarola</a></Link>
+                                <Link href="/"><a target="_blank">Espárragos verdes</a></Link>
+                                <Link href="/"><a target="_blank">Coliflor</a></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Carnes</p>
+                                <Image src="/img/meat.svg" width={50} height={50} alt="Carnes para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a target="_blank">Pollo</a></Link>
+                                <Link href="/"><a target="_blank">Ternera</a></Link>
+                                <Link href="/"><a target="_blank">Higado</a></Link>
+                                <Link href="/"><a target="_blank">Carne cruda</a></Link>
+                                <Link href="/"><a target="_blank">Croquetas</a></Link>
+                                <Link href="/"><a target="_blank">Bocadillo</a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Legumbres</p>
+                                <Image src="/img/red-bean.svg" width={50} height={50} alt="Legumbres para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a target="_blank">Guisantes</a></Link>
+                                <Link href="/"><a target="_blank">Garbanzos</a></Link>
+                                <Link href="/"><a target="_blank">Alubias</a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Dulces</p>
+                                <Image src="/img/cake.svg" width={50} height={50} alt="Dulces para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a target="_blank">Croissant</a></Link>
+                                <Link href="/"><a target="_blank">Copos de avena</a></Link>
+                                <Link href="/"><a target="_blank">Chocolate</a></Link>
+                                <Link href="/"><a target="_blank">Dulce de leche</a></Link>
+                                <Link href="/"><a target="_blank">Galletas saladas</a></Link>
+                                <Link href="/"><a target="_blank">Ensaimadas</a></Link>
+                                <Link href="/"><a target="_blank">Flan</a></Link>
+                                <Link href="/"><a target="_blank">Bombones</a></Link>
+                            </div>
+                        </div>
+                        <div>
+                            <div className={s.popularTitle}>
+                                <p>Bebidas</p>
+                                <Image src="/img/coke-can.svg" width={50} height={50} alt="Bebidas para perros" />
+                            </div>
+                            <div>
+                                <Link href="/"><a target="_blank">Cerveza</a></Link>
+                                <Link href="/"><a target="_blank">Coca Cola</a></Link>
+                                <Link href="/"><a target="_blank">Hielo</a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                                <Link href="/"><a target="_blank"></a></Link>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
         </>
     )
+}
+
+export async function getServerSideProps() {
+
+    let {data} = await supabase.from('Ingredientes').select(`ID, nombre, descripcion, url`);
+    let ingredients = data;
+    return {
+        props: { ingredients }
+    }
 }
