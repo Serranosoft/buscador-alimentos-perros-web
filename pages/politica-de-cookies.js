a5a3456ab7f32cb36dd9ab11e87374e6004aebcd
Manuel Scholz <manuel.serrano943@gmail.com>
2022-08-22 18:55:52 +0200

    Feat: Estilos politicas

--- pages/politica-de-cookies.js
+++ pages/politica-de-cookies.js
@@ -1,12 +1,24 @@
 import { getPageById } from './api/wordpress';
 
-export default function PoliticaDeCookies({data}) {
+export default function PoliticaDeCookies({ data }) {
 
     return (
-        <>
-            <h1>{data.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: data.content }} />
-        </>
+        <section
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "0 3rem"
+            }}>
+            <h1 style={{
+                textAlign: "center",
+                fontSize: "4rem"
+            }}>{data.title}</h1>
+            <div style={{
+                maxWidth: "1020px"
+            }}
+                dangerouslySetInnerHTML={{ __html: data.content }} />
+        </section>
     )
 }
 
