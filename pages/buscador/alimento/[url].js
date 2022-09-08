c8bb5e486e511c20b91227150b4c4a14d55c5f76
Manuel Scholz <manuel.serrano943@gmail.com>
2022-09-08 23:37:34 +0200

    Fix: Eliminar loadingBar y generar correctamente el html estatico de todas las urls

--- pages/buscador/alimento/[url].js
+++ pages/buscador/alimento/[url].js
@@ -2,7 +2,6 @@ import s from '../../../styles/css/[url].module.css'
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
 import { supabase } from '../../../utils/supabaseClient';
-import Loading from '../../../components/Loading';
 import { addSpaces } from '../../../utils/strings';
 import Suggestion from '../../../components/suggestions';
 import Link from 'next/link';
@@ -18,44 +17,38 @@ export default function Ingrediente({ data }) {
 
     return (
         <>
-            {!ready ?
-                <Loading />
-                :
-                <>
-                    <Head>
-                        <title>¿Un perro puede comer {data.nombre}? Respuestas rápidas en BuscaDog</title>
-                        <meta name="description" content={`¿Un perro puede comer ${data.nombre}? Respuestas rápidas en Buscadog`} />
-                        <meta property="og:title" content={`¿Un perro puede comer ${data.nombre}? Respuestas rápidas en Buscadog`} />
-                        <meta property="og:url" content={`https://buscadog.net/buscador/alimento/${data.url}`} />
-                    </Head>
-                    <main className={s.root}>
-                        <section>
+            <Head>
+                <title>¿Un perro puede comer {data.nombre}? Respuestas rápidas en BuscaDog</title>
+                <meta name="description" content={`¿Un perro puede comer ${data.nombre}? Respuestas rápidas en Buscadog`} />
+                <meta property="og:title" content={`¿Un perro puede comer ${data.nombre}? Respuestas rápidas en Buscadog`} />
+                <meta property="og:url" content={`https://buscadog.net/buscador/alimento/${data.url}`} />
+            </Head>
+            <main className={s.root}>
+                <section>
+                    <div>
+                        <div className={s.content}>
+                            <h1>¿Los perros pueden comer <span>{data.nombre}</span>?</h1>
                             <div>
-                                <div className={s.content}>
-                                    <h1>¿Los perros pueden comer <span>{data.nombre}</span>?</h1>
-                                    <div>
-                                        <p className={s.description}><span>{data.respuesta} .</span>{data.descripcion}</p>
-                                    </div>
-                                    <Link href="/buscador/alimento">
-                                        <a className={s.back}>
-                                            <div>
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M151.5 347.8L3.5 201c-4.7-4.7-4.7-12.3 0-17l19.8-19.8c4.7-4.7 12.3-4.7 17 0L160 282.7l119.7-118.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17l-148 146.8c-4.7 4.7-12.3 4.7-17 0z"></path></svg>
-                                            </div>
-                                            <span>Buscar otros alimentos</span>
-                                        </a>
-                                    </Link>
-                                    <div className={s.notification}>
-                                        <p>¡Te recomendamos visitar a un veterinario antes de tomar una decisión para obtener una recomendación respaldada por un profesional!</p>
-                                    </div>
-                                </div>
-                                <div className={s.sidebar}>                                    
-                                    <Suggestion />
-                                </div>
+                                <p className={s.description}><span>{data.respuesta} .</span>{data.descripcion}</p>
                             </div>
-                        </section>
-                    </main>
-                </>
-            }
+                            <Link href="/buscador/alimento">
+                                <a className={s.back}>
+                                    <div>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M151.5 347.8L3.5 201c-4.7-4.7-4.7-12.3 0-17l19.8-19.8c4.7-4.7 12.3-4.7 17 0L160 282.7l119.7-118.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17l-148 146.8c-4.7 4.7-12.3 4.7-17 0z"></path></svg>
+                                    </div>
+                                    <span>Buscar otros alimentos</span>
+                                </a>
+                            </Link>
+                            <div className={s.notification}>
+                                <p>¡Te recomendamos visitar a un veterinario antes de tomar una decisión para obtener una recomendación respaldada por un profesional!</p>
+                            </div>
+                        </div>
+                        <div className={s.sidebar}>                                    
+                            <Suggestion />
+                        </div>
+                    </div>
+                </section>
+            </main>
         </>
 
 
@@ -64,7 +57,7 @@ export default function Ingrediente({ data }) {
 
 export async function getStaticPaths() {
     let { data } = await supabase.from("Ingredientes").select("url");
-
+    console.log(data);
     const paths = data.map((ingr) => ({
         params: { url: ingr["url"].toLowerCase() }
     }))
