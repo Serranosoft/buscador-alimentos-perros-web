1c0957edfc38f3281b1045142d7aaa193ae4c874
Manuel Scholz <manuel.serrano943@gmail.com>
2023-05-27 18:34:38 +0200

    Feat: Eliminar código innecesario

--- src/pages/buscador/index.js
+++ src/pages/buscador/index.js
@@ -1,23 +1,23 @@
-import Head from "next/head";
-import s from '../../styles/css/BuscadorIngrediente.module.css'
+// import Head from "next/head";
+// import s from '../../styles/css/BuscadorIngrediente.module.css'
 
-export default function BuscadorAlimentos() {
+// export default function BuscadorAlimentos() {
 
-    return (
-        <>
-            <Head>
-                <title>Página web en construcción.</title>
-                <meta name="description" content={`Página web en construcción.`} />
-                <meta property="og:title" content={`Página web en construcción.`} />
-                <meta name="twitter:description" content="Página web en construcción."></meta>
-            </Head>
-            <section>
-                <div className={s.container}>
-                    <div className={s.inputContainer}>
-                        <h1>Sección en construcción...</h1>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
-}
+//     return (
+//         <>
+//             <Head>
+//                 <title>Página web en construcción.</title>
+//                 <meta name="description" content={`Página web en construcción.`} />
+//                 <meta property="og:title" content={`Página web en construcción.`} />
+//                 <meta name="twitter:description" content="Página web en construcción."></meta>
+//             </Head>
+//             <section>
+//                 <div className={s.container}>
+//                     <div className={s.inputContainer}>
+//                         <h1>Sección en construcción...</h1>
+//                     </div>
+//                 </div>
+//             </section>
+//         </>
+//     )
+// }
