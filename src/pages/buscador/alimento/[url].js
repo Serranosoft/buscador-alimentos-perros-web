196a8ac098f0f4cd7cdfc4027d5d09f581b7414d
Manuel Scholz <manuel.serrano943@gmail.com>
2023-05-31 19:31:48 +0200

    Feat: Estilos para los alimentos

--- src/pages/buscador/alimento/[url].js
+++ src/pages/buscador/alimento/[url].js
@@ -8,6 +8,7 @@ import { getAllSlugs, getPostBySlug, getTitleItems } from '@/pages/api/wordpress
 import Container from '@/components/content/container';
 import Searcher from '@/components/searcher/searcher';
 import Breadcrumbs from '@/components/breadcrumbs/breadcrumbs';
+import Muted from '@/components/content/muted';
 
 export default function Ingrediente({ data, ingredients }) {
 
@@ -23,15 +24,15 @@ export default function Ingrediente({ data, ingredients }) {
             <Container className={styles.root}>
                 <div>
                     <div className={styles.content}>
-                        <Breadcrumbs />
-                        <H1>¿Los perros pueden comer <span>{data.title.toLowerCase()}</span>?</H1>
-                        <div dangerouslySetInnerHTML={{ __html: data.content }}></div>
-                        {/* <Link href="/buscador/alimento" className={styles.back}>
+                        <div className={styles.header}>
+                            <Breadcrumbs />
+                            <H1>¿Los perros pueden comer <span>{data.title.toLowerCase()}</span>?</H1>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M151.5 347.8L3.5 201c-4.7-4.7-4.7-12.3 0-17l19.8-19.8c4.7-4.7 12.3-4.7 17 0L160 282.7l119.7-118.5c4.7-4.7 12.3-4.7 17 0l19.8 19.8c4.7 4.7 4.7 12.3 0 17l-148 146.8c-4.7 4.7-12.3 4.7-17 0z"></path></svg>
+                                <img src="/favicon.svg" />
+                                <Muted>{data.author.node.name}</Muted>
                             </div>
-                            <span>Buscar otros alimentos</span>
-                        </Link> */}
+                        </div>
+                        <div dangerouslySetInnerHTML={{ __html: data.content }}></div>
                         <div className={styles.notification}>
                             <Text>¡Te recomendamos visitar a un veterinario antes de tomar una decisión para obtener una recomendación respaldada por un profesional!</Text>
                         </div>
